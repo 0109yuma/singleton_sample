8dae3b690f6e92a226fb82d5c9048f927410ba93
Yuma Oda <wyc00nny@gmail.com>
2014-05-03 23:38:26 +0900

    Initial Commit

---
+++ Singleton/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Singleton
+//
+//  Created by oda yuma on 2014/05/03.
+//  Copyright (c) 2014年 yuma oda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ SingletonTests/SingletonTests.m
@@ -0,0 +1,34 @@
+//
+//  SingletonTests.m
+//  SingletonTests
+//
+//  Created by oda yuma on 2014/05/03.
+//  Copyright (c) 2014年 yuma oda. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SingletonTests : XCTestCase
+
+@end
+
+@implementation SingletonTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
