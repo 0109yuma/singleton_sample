2f290bac0c0559de8c5a38fba717204b425674fe
Yuma Oda <wyc00nny@gmail.com>
2014-05-03 23:40:11 +0900

    SingletonSampleクラス追加

---
+++ Singleton/SingletonSample.h
@@ -0,0 +1,13 @@
+//
+//  SingletonSample.h
+//  Singleton
+//
+//  Created by oda yuma on 2014/05/03.
+//  Copyright (c) 2014年 yuma oda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SingletonSample : NSObject
+
+@end
---
+++ Singleton/SingletonSample.m
@@ -0,0 +1,13 @@
+//
+//  SingletonSample.m
+//  Singleton
+//
+//  Created by oda yuma on 2014/05/03.
+//  Copyright (c) 2014年 yuma oda. All rights reserved.
+//
+
+#import "SingletonSample.h"
+
+@implementation SingletonSample
+
+@end
